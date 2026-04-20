d7bf32d3e2895d29d29a05a90e886b85be086275
codecompiler19-sketch <codecompiler19@gmail.com>
2026-04-21 03:07:11 +0530

    SQL

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const tutorialsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
-    category: z.enum(['html', 'css', 'javascript', 'seo', 'python']),
+    category: z.enum(['html', 'css', 'javascript', 'seo', 'python', 'sql']),
     order: z.number(),
   }),
 });
