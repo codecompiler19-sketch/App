2cd05fdc8bb4eb72b96e99966e9aba66e22ca065
codecompiler19-sketch <codecompiler19@gmail.com>
2026-04-24 01:33:26 +0530

    admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap()],
   site: 'https://codescompiler.com',
 });
